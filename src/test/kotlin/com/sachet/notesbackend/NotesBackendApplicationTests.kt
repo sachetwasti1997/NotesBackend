005c53660de89fcac3214c67623c2660bc666d7e
sachetwasti1997 <sachetwasti61@gmail.com>
2022-05-05 11:03:58 +0530

    Initialised the project

---
+++ src/test/kotlin/com/sachet/notesbackend/NotesBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sachet.notesbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NotesBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
